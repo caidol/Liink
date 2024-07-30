dc3d6efeb163cad279f0ee04e2ba9a3ce777386e
Myriad74 <acatchpool@protonmail.com>
2024-07-30 11:10:47 +0100

    create vacant channel animation and apply channel mask

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const images = ['../assets/channels/static1.png', '../assets/channels/static2.png', '../assets/channels/static3.png', '../assets/channels/static4.png'];
+let currentIndex = 0;
+
+function changeImage() {
+    const elements = document.querySelectorAll('.channel .channel-vacant');
+    
+    elements.forEach(element => {
+        console.log(`Setting image ${images[currentIndex]}`);
+        element.style.setProperty('--background-image', `url(${images[currentIndex]})`);
+    });
+    
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+setInterval(changeImage, 75); 
